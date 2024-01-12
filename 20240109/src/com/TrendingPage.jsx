cb96bef03aa50bfe003e31166a999d4fd16d46bc
sikkkkkw <gksmsla92@naver.com>
2024-01-12 19:35:30 +0900

    npm update

--- 20240109/src/com/TrendingPage.jsx
+++ 20240109/src/com/TrendingPage.jsx
@@ -36,7 +36,7 @@ export default function TrendingPage() {
 
   return (
     <div className="w-full flex justify-center bg-[#1B354A]">
-      <div className="w-[1300px] h-[450px] pt-8 ">
+      <div className="w-[1300px] h-[400px] pt-8 ">
         {/* 타이틀 */}
         <h2 className="px-4 py-2 font-semibold text-[24px]">Trending</h2>
         {/* 리스트 */}
